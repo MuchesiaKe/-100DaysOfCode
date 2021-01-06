ee8313571382bc5caf737fe9bbc13b0e8e2a0568
Wallace Muchesia <muchesiawallace@gmail.com>
2021-01-06 16:50:56 +0300

    Aced the translator program

---
+++ translator.py
@@ -0,0 +1,13 @@
+def translator(phrase):
+    translation = ""
+    for letter in phrase:
+        if letter in "AEIOUaeiou":
+            if letter.isupper():
+                translation = translation + "G"
+            else:
+                 translation = translation + "g"
+
+        else:
+            translation = translation + letter
+    return translation
+print(translator(input("Enter phrase to translate: ")))
