16f860a7d6c3256566d3fc61b59f249dfe46a4ef
Wallace Muchesia <muchesiawallace@gmail.com>
2021-01-06 14:47:11 +0300

    guessinggame.py progress

--- guessinggame.py
+++ guessinggame.py
@@ -6,14 +6,13 @@
 #If the user gives three guesses unsuccessfull, print a fail message
 
 secret_word = "Antelope"
-guess_attempt = 3
-right = False
-
-while guess_attempt ==0 and right==True:
-   guess_word= input("Enter guess word: ")
-    guess_attempt -=1
-    if guess_word == secret_word:
-        print("You are absolutely correct!")
-        right = True
-    else:
-        print("You lost!")
+guess = ""
+guess_count=0
+guess_over=False
+while secret_word != guess and guess_over == False:
+    guess = input("Enter guess word: ")
+    guess_count +=1
+    if guess_count >2:
+        guess_over = True
+        print("You lost the game!")
+print("Correct!")
