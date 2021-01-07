9d9384ea1e4b43587179d5e0115739965ef3b18c
Wallace Muchesia <muchesiawallace@gmail.com>
2021-01-07 10:11:53 +0300

    Illustrating readline() function

---
+++ readingfiles.py
@@ -0,0 +1,12 @@
+read_file = open("translator.py", "r")
+
+print(read_file.readline())
+print(read_file.readline())
+print(read_file.readline())
+print(read_file.readline())
+print(read_file.readline())
+print(read_file.readline())
+print(read_file.readline())
+print(read_file.readline())
+
+read_file.close()
--- tryexcept.py
+++ tryexcept.py
@@ -1,5 +1,8 @@
 try:
+    value = 10/0
     number = int(input("Enter a number: "))
     print(number)
-except:
-    print("Invalid input")
+except ValueError as err:
+    print(err)
+except ZeroDivisionError as zero: #store the error in a variable
+    print(zero)
