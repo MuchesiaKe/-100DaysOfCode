3b471dd59628fe95fe6e9b09bcd915160c407eef
Wallace Muchesia <muchesiawallace@gmail.com>
2021-01-08 09:34:46 +0300

    Implemented simple class program by importing from external program

---
+++ class_implementation.py
@@ -0,0 +1,7 @@
+from external_class import employees
+
+employee1 = employees(1, "John Doe", "Planning and Engineering", "Network Administrator")
+employee2 = employees(34, "William Kamau", "Network Operations Center", "Network Support")
+employee87 = employees (872, "Jane Nafula", "Customer Service", "Customer Support")
+
+print(employee1.department)
---
+++ external_class.py
@@ -0,0 +1,7 @@
+class employees:
+    def __init__ (self, emp_no, name, department, designation):
+        self.emp_no = emp_no
+        self.name = name
+        self.department = department
+        self.designation = designation
+        
